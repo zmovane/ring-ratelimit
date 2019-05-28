e97dbd329953efaf275991fd1fac32db0cded429
nyakokishi <860321158@qq.com>
2019-05-28 16:32:37 +0800

    format code

--- src/main/clojure/middleware/ratelimit/backend.clj
+++ src/main/clojure/middleware/ratelimit/backend.clj
@@ -5,26 +5,19 @@
   (get-bucket [self bucket-key])
   (update-bucket [self bucket-key bucket]))
 
-
-(deftype RedisBackend [redis-conf hash-key] Backend
-
-  (get-bucket [self bucket-key]
-    (car/wcar redis-conf
-      (car/hget hash-key bucket-key)))
-
-  (update-bucket [self bucket-key bucket]
-    (car/wcar redis-conf
-      (car/hset hash-key bucket-key bucket))))
-
-
 (deftype LocalAtomBackend [buckets] Backend
-
   (get-bucket [self bucket-key]
     (get @buckets bucket-key))
-
   (update-bucket [self bucket-key bucket]
     (swap! buckets assoc bucket-key bucket)))
 
+(deftype RedisBackend [redis-conf hash-key] Backend
+  (get-bucket [self bucket-key]
+    (car/wcar redis-conf
+      (car/hget hash-key bucket-key)))
+  (update-bucket [self bucket-key bucket]
+    (car/wcar redis-conf
+      (car/hset hash-key bucket-key bucket))))
 
 (def ^:private default-buckets (atom {}))
 
@@ -33,7 +26,6 @@
    (local-atom-backend default-buckets))
   ([buckets]
    (LocalAtomBackend. buckets)))
-
 
 (def ^:private default-opts {:pool {} :spec {:host "127.0.0.1" :port 6379 :db 0}})
 
